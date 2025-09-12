b07bdfbc716916b89c2f505e24d00852215e924c
DEAD-SHOCK <krystiankusmierz6@gmail.com>
2025-09-12 12:50:35 +0200

    added templates

---
+++ custom_elements.js
@@ -0,0 +1,31 @@
+
+class nav extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML=
+        ` <nav>
+        <div class="button"><a href="characters.html">characters</a></div>
+        <div class="button"><a href="world.html">world</a></div>
+        <div class="button"><a href="">story</a></div>
+        <div class="button"><a href="">artifacts</a></div>
+    </nav>
+    
+    `
+    }
+}
+
+class footer extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML=
+        `       <footer> Property of SHOCK MEDIA</footer>     `
+    }
+}
+class header extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML=
+        `           <header style="color=white"><a href="index.html">KYODAI</a></header>
+     `
+    }
+}
+customElements.define('my-header', header);
+customElements.define('my-footer', footer);
+customElements.define('nav-bar', nav);
